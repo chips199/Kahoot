19665fd93a1105aa8dfe76873553b0240248af80
Chips199 <chips199@gmx.de>
2020-03-12 14:49:03 +0100

    V0.5 Klasse Question erstellt

--- src/client/objects/Question.java
+++
@@ -1,5 +0,0 @@
-package client.objects;
-
-public class Question {
-
-}
---
+++ src/server/objects/Questions.java
@@ -0,0 +1,27 @@
+package server.objects;
+
+public class Questions {
+	
+	private String question;
+	private String[] answer;
+		
+	public Questions(String question, String[] answer) {
+		super();
+		this.question = question;
+		this.answer = answer;
+	}
+	
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String[] getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String[] answer) {
+		this.answer = answer;
+	}
+	
+}
