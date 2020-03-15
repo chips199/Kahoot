ec7e8562463e53c398aa64fc1cdbe9d0578fa2f9
Chips199 <chips199@gmx.de>
2020-03-15 22:57:46 +0100

    V0.7 Server-Klasse editiert

--- src/server/main/Server.java
+++ src/server/main/Server.java
@@ -5,15 +5,18 @@ import java.util.ArrayList;
 import server.gui.ServerGUI;
 import server.objects.Player;
 import server.objects.Question;
+import server.objects.Connection;
 
 public class Server {
 	private ArrayList<Question> questions;
 	private ArrayList<Player> players;
 	private ServerGUI gui;
+	private Connection con;
 	
-	public Server() {
+	public Server(String ip, int port) {
 		questions = new ArrayList<Question>();
 		players = new ArrayList<Player>();
+		con = new Connection(ip, port);
 	}
 	
 	public boolean addPlayer(Player player) {
@@ -26,5 +29,9 @@ public class Server {
 	
 	public boolean addQuestion(Question question) {
 		return questions.add(question);
+	}
+	
+	public ArrayList<Question> getQuestions(){
+		return questions;
 	}
 }
