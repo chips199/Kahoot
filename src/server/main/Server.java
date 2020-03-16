62258b03176c0e4adadc2fe50c43286429dda562
chips199 <chips199@gmx.de>
2020-03-16 10:29:24 +0100

    V0.7 Protokollliste erweitert + Server bearbeitet

--- src/server/main/Server.java
+++ src/server/main/Server.java
@@ -2,21 +2,54 @@ package server.main;
 
 import java.util.ArrayList;
 
-import server.classes.Connection;
+import server.classes.MainServer;
 import server.gui.ServerGUI;
 import server.objects.Player;
 import server.objects.Question;
 
-public class Server {
+public class Server extends MainServer {
 	private ArrayList<Question> questions;
 	private ArrayList<Player> players;
 	private ServerGUI gui;
-	private Connection con;
 	
-	public Server(String ip, int port) {
+	public Server(int port) {
+		super(port);
 		questions = new ArrayList<Question>();
 		players = new ArrayList<Player>();
-		con = new Connection(ip, port);
+	}
+	
+	/**
+	 * Eine neue Verbindung verarbeiten:
+	 * Wenn ein neuer Nutzer kommt: Player erstellen & WELCOMEBACK-Nachricht
+	 * sonst Willkomensnachricht
+	 */
+	public void processNewConnection(String pClientIP, int pClientPort) {
+		boolean newPlayer = true;
+		for(Player player: players) {
+			if(player.getIp().equals(pClientIP)) {
+				send(pClientIP, pClientPort, "WELCOMEBACK:" + player.getName() + ":" + player.getPoints());
+				newPlayer = false;
+				break;
+			}
+		}
+		if(newPlayer) {
+			players.add(new Player(pClientIP,pClientIP, 0));
+			send(pClientIP, pClientPort, "WELCOME:" + pClientIP + ":0");
+		}
+	}
+	
+	public void processMessage(String pClientIP, int pClientPort, String pMessage) {
+		
+		// Dem Nutzer einen neuen Namen vergeben
+		if (pMessage.startsWith("CHANGENAME:")) {
+			String name = pMessage.split(":")[1];
+			for(Player player:players) {
+				if(player.getIp().equals(pClientIP)) {
+					player.setName(name);
+					break;
+				}
+			}
+		}
 	}
 	
 	public boolean addPlayer(Player player) {
