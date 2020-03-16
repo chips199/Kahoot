56cbbaa538904c4c85f7a13d0c28f1c0bfb15a7b
chips199 <chips199@gmx.de>
2020-03-16 12:19:39 +0100

    0.10 Umstrukturierung
    Controller jetzt als Hauptklasse
    neue Protkollbefehle hinzugefügt
    Server-Klasse angepasst
    Klasse Player: Instanzvariable Port hinzugefügt

---
+++ src/server/main/Controller.java
@@ -0,0 +1,93 @@
+package server.main;
+
+import java.util.ArrayList;
+
+import server.gui.ServerGUI;
+import server.objects.Player;
+import server.objects.Question;
+
+public class Controller {
+	private ArrayList<Question> questions;
+	private ArrayList<Player> players;
+	private ServerGUI gui;
+	private Server server;
+	private long startQuestionTime;
+	
+	public Controller (int port) {
+		questions = new ArrayList<Question>();
+		players = new ArrayList<Player>();
+		server = new Server(port, this);
+	}
+	
+	/**
+	 * Methode um das Quiz zu starten
+	 */
+	public void startQuiz() {
+		for(Question question: questions) {
+			String [] mixedAnswers = mixAnswers(question.getAnswer());
+			server.sendToAll("SENDANSWERS:" + mixedAnswers[0] + ":" + mixedAnswers[1] + ":" + mixedAnswers[2] + ":" + mixedAnswers[3]);
+			startQuestionTime = System.currentTimeMillis();
+			do {
+				try {
+					Thread.sleep(10);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} while (startQuestionTime + 10000 <= System.currentTimeMillis() || everyoneHasAnswered());
+			sendPointsToEach();
+		}
+	}
+	
+	/**
+	 * Überprüft ob jeder auf die Frage geantwortet hat.
+	 * @return boolean
+	 */
+	public boolean everyoneHasAnswered() {
+		
+		return false;
+	}
+	
+	public boolean checkAnswer(int answer) {
+		return false;
+	}
+	
+	public String [] mixAnswers(String [] answers) {
+		return answers;
+	}
+	
+	/**
+	 * Sendet den aktuellen Punktestand an die jeweiligen Spieler
+	 */
+	public void sendPointsToEach() {
+		for(Player player: players) {
+			server.send(player.getIp(), player.getPort(), "SENDPOINTS:" + player.getPoints());
+		}
+	}
+	
+	/**
+	 * Getter und Setter-Methoden der Instanzvariable
+	 */
+	public boolean addPlayer(Player player) {
+		return players.add(player);
+	}
+	
+	public ArrayList<Player> getPlayers(){
+		return players;
+	}
+	
+	public boolean addQuestion(Question question) {
+		return questions.add(question);
+	}
+	
+	public ArrayList<Question> getQuestions(){
+		return questions;
+	}
+	
+	public void setStartQuestionTime (long startQuestionTime) {
+		this.startQuestionTime = startQuestionTime;
+	}
+	
+	public long getStartQuestionTime () {
+		return startQuestionTime;
+	}
+}
